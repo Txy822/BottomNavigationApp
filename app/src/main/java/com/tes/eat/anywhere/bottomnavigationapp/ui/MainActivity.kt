a070449fac44657c60605a9e8cc052c43861166b
tesfahun <y.w.tesfahun@gmail.com>
2022-10-14 22:11:48 +0100

    application for dependancy injectiopn added

--- app/src/main/java/com/tes/eat/anywhere/bottomnavigationapp/ui/MainActivity.kt
+++ app/src/main/java/com/tes/eat/anywhere/bottomnavigationapp/ui/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.tes.eat.anywhere.bottomnavigationapp
+package com.tes.eat.anywhere.bottomnavigationapp.ui
 
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -7,6 +7,7 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.tes.eat.anywhere.bottomnavigationapp.R
 import com.tes.eat.anywhere.bottomnavigationapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
