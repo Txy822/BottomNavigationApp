ecbcad7b7d17ab506e0fec3201e6e32a1fe3d484
tesfahun <y.w.tesfahun@gmail.com>
2022-10-14 21:51:16 +0100

    dependencies and plugin added

---
+++ app/src/main/java/com/tes/eat/anywhere/bottomnavigationapp/App.kt
@@ -0,0 +1,8 @@
+package com.tes.eat.anywhere.bottomnavigationapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+import kotlin.text.Typography.dagger
+
+@HiltAndroidApp
+class App : Application()
